22de72e65efcf9791f414769220e13907f6529bd
nusrat-git <nusratjahanmithila66@gmail.com>
2023-01-11 18:48:26 +0600

    done

---
+++ controllers/auth.js
@@ -0,0 +1,20 @@
+const User = require("../models/user");
+
+exports.getLogin = (req, res, next) => {
+  console.log(req.session.isLoggedIn);
+  res.render("auth/login", {
+    path: "/login",
+    pageTitle: "Login",
+    isAuthenticated: false,
+  });
+};
+
+exports.postLogin = (req, res, next) => {
+  User.findById("63beab8ccc10230548ecdbc8")
+    .then((user) => {
+      req.session.isLoggedIn = true;
+      req.session.user = user;
+      res.redirect("/");
+    })
+    .catch((err) => console.log(err));
+};
